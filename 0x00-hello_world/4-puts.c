e51d5476cd780190fb2a7854e3a7d11af86a1441
joetetteh <tettehj360@gmail.com>
2023-03-16 19:50:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
- * prorame to print a line with put function
- * Return: 0
+/*
+ * A betty programe that print a line with put function
+ * Return: 0 (Success)
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");      
- return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
