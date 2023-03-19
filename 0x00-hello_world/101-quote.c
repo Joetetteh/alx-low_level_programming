d2f19c79055b8d3dea1bf1110766b09431abfbaf
joetetteh <tettehj360@gmail.com>
2023-03-19 10:32:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
-
 /**
- * main - c program wihout the use of print f or puts functions
+ * main -  C program that prints exactly and that piece of art is useful
  *
- * Return: 1 (Success)
+ * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-	writ(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
