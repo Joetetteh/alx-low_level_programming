7e6d1dd458409906866570f257d48abbaeedea14
joetetteh <tettehj360@gmail.com>
2023-03-21 16:35:07 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,15 @@
  *
  * Return: Always (Success)
  */
+
 void print_alphabet_x10(void)
 {
 char alph;
+int i;
+for (i = 1; i <= 10; i++)
+{
 for (alph = 'a'; alph <= 'z'; alph++)
 _putchar(alph);
 _putchar('\n');
+}
 }
