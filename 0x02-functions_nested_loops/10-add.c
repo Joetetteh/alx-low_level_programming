cd073516cfea02868dd5712eb151bbd39588551c
joetetteh <tettehj360@gmail.com>
2023-03-22 19:24:02 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - a function to add two numbers
+ *
+ * @a: function para
+ *
+ * @b: function para
+ *
+ * Return: Always (Success)
+ */
+
+int add(int a, int b)
+{
+int result;
+
+result = a + b;
+return (result);
+}
