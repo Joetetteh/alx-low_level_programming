e844ca3d1b7ab00ae75b91c4295b0cc7b6bdf97a
joetetteh <tettehj360@gmail.com>
2023-04-02 11:49:38 -0700

    6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,10 +3,36 @@
 /**
  * cap_string - a function to capitalize all words of strings
  *
- * @*: parameter
+ * @str: parameter
  *
  * Return: value
  *
  */
 
+char *cap_string(char *str)
+{
+int i;
 
+while (str[i])
+{
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+i++;
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == ':' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
+i == 0)
+str[i] -= 32;
+i++;
+}
+return (0);
+}
